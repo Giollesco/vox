effe36c578675dacb236e46e60cbf44b9c4a3a07
Giollesco <ivandjolo@gmail.com>
2024-09-28 14:55:20 +0200

    feat(dashboard): :recycle: logout button refactored

---
+++ src/components/dashboard/logout-button/index.tsx
@@ -0,0 +1,32 @@
+import { useAuth } from '@/core';
+import { Text, View } from '@/ui';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics';
+
+export default function LogoutButton() {
+  // Hooks
+  const auth = useAuth();
+
+  // Functions
+  function handleLogout() {
+    auth.signOut();
+    impactAsync(ImpactFeedbackStyle.Heavy);
+  }
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={handleLogout}
+      style={{ opacity: 0.5 }}
+    >
+      <View className="flex-1 items-center justify-center pt-2">
+        <MaterialIcons name="logout" size={18} color="black" />
+        <Text className="text-center" style={{ fontSize: 10 }}>
+          Odjava
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}

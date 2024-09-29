93da7cc51ef93be99187e7b9653fe286749eaa90
Giollesco <ivandjolo@gmail.com>
2024-09-29 17:15:12 +0200

    fix(dashboard): :adhesive_bandage: alert prompt implemented on logout action

--- src/components/dashboard/logout-button/index.tsx
+++ src/components/dashboard/logout-button/index.tsx
@@ -2,7 +2,7 @@ import { useAuth } from '@/core';
 import { Text, View } from '@/ui';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics';
 
 export default function LogoutButton() {
@@ -15,8 +15,21 @@ export default function LogoutButton() {
     impactAsync(ImpactFeedbackStyle.Heavy);
   }
 
+  const createTwoButtonAlert = () =>
+    Alert.alert('Odjava', 'Jeste li sigurni da se želite odjaviti?', [
+      {
+        text: 'Odustani',
+        style: 'cancel',
+      },
+      {
+        text: 'Odjava',
+        style: 'destructive',
+        onPress: handleLogout,
+      },
+    ]);
+
   return (
-    <TouchableOpacity activeOpacity={0.8} onPress={handleLogout}>
+    <TouchableOpacity activeOpacity={0.8} onPress={createTwoButtonAlert}>
       <View className="flex-1 items-center justify-center pt-2">
         <MaterialIcons
           name="logout"
